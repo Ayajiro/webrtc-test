9c4bb49447d0fcbc6e2a651c97dbbb049cf5410d
Ayajiro <nakamura.ayajiro@gmail.com>
2025-02-07 23:38:17 +0800

    test libs

--- apps/back-end/src/main.ts
+++ apps/back-end/src/main.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import * as lib from '@webrtc-test/libs';
+
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
@@ -12,3 +14,8 @@ app.get('/', (req, res) => {
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
+
+
+lib.libs();
+
+
